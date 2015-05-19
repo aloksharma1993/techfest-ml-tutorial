b1b1e9a8615f1b26d12a5230265443b896274a2f
Arsen Mamikonyan <arsen@mamikonyan.am>
2015-05-19 14:37:50 -0700

    setup...

---
+++ setup/prettify_ports.py
@@ -0,0 +1,15 @@
+import os
+import csv
+
+if __name__ == '__main__':
+  _, nodename, _, _, _ = os.uname()
+  with open('setup/ports.csv') as infile:
+    reader = csv.reader(infile)
+    with open('setup/pretty_ports.csv', 'w') as outfile:
+      writer = csv.writer(outfile)
+      for port, password in reader:
+        writer.writerow(['ipython notebook server url', '', 'password'])
+        writer.writerow(['', '', ''])
+        writer.writerow(['https://{node}:{port}'.format(node=nodename, port=port), '', password])
+        writer.writerow(['', '', ''])
+        writer.writerow(['', '', ''])
